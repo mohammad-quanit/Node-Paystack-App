2c54d3cca89b47c0740ec440f1c795d7a50138ce
Mohammad Quanit <32056694+Mohammad-Quanit@users.noreply.github.com>
2020-01-11 15:55:14 +0500

    Update mongoose.js

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 mongoose.connect(
-  'mongodb://mquanit:danzakuduro12345@ds113853.mlab.com:13853/starter-db', {useNewUrlParser: true}
+  'url-string-heere', {useNewUrlParser: true}
 );
 
 module.exports = { mongoose };
